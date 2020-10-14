1cd26226a1a040c2ee6df167f18014a50a5d902a
TomasMoratoPerezPorro <49519581+TomasMoratoPerezPorro@users.noreply.github.com>
2020-10-14 17:27:47 +0200

    Local database settings

--- database.js
+++ database.js
@@ -11,28 +11,14 @@ var pool = mysql.createPool({
   multipleStatements: true,
 });
 
-/* module.exports = pool; */
 
-/* const mysqlConnection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE,
-  port: 3306,
-  dateStrings: true,
-  multipleStatements: true,
-});
-
-mysqlConnection.connect(function (err) {
-  if (err) {
-    console.log(err);
-    return;
-  } else {
-    console.log("Db is connected");
-  }
-}); */
-
-/* module.exports.mysqlConnection = mysqlConnection; */
 module.exports.pool = pool;
 
-/* module.exports = mysqlConnection; */
+/* const mysqlConnection = mysql.createConnection({
+  host: "localhost",
+  user:"root",
+  password:"password",
+  database:"restaurant",
+  dateStrings: true,
+  multipleStatements:true
+}); */
